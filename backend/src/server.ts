c2b020437800a8a649def92c6930d95ebebaf71b
Braeden Hintze <bhintze@lucidchart.com>
2025-07-25 14:08:33 -0600

    Use typescript in the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
@@ -11,7 +11,9 @@ const io = new Server(server, {
   }
 });
 
-app.get('/', (req, res) => {
+import { Request, Response } from 'express';
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Couch Game backend is running.');
 });
 
